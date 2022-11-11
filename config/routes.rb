fff94c21bbad71a611d63534bd2abdfa7bcd83a5
Alex Trimper <trimper.a@gmail.com>
2022-11-11 11:48:19 -0400

    corrected routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  # get 'lists', to: 'lists#index'
-  # get 'lists/:id', to: 'lists#show', as: :list
-  # get 'lists/new', to: 'list#new', as: :new_list
-  # post 'lists', to: 'lists#create'
+  root 'lists#index'
 
-  # resources :lists do
-  #   collection do
-  #     get :list
-  #   end
-
-  #   member do
-  #     get :movie
-  #   end
-  # end
-
+  resources :lists
 end
